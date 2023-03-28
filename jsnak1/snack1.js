f7b446f76458cb48bd450616df103af05a834555
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-28 10:55:53 +0200

    snack 1 - concluso

---
+++ jsnak1/snack1.js
@@ -0,0 +1,17 @@
+// l''utente inserisce due numeri in sucessione, con due prompt. il software stampa il maggiore
+
+
+// chiedi all'utente di inserire il primo numero
+let numero1 = prompt("Inserisci il primo numero:");
+
+// chiedi all'utente di inserire il secondo numero
+let numero2 = prompt("Inserisci il secondo numero:");
+
+// controlla quale numero è maggiore
+if (numero1 > numero2) {
+    console.log("Il numero maggiore è: " + numero1);
+  } else if (numero2 > numero1) {
+    console.log("Il numero maggiore è: " + numero2);
+  } else {
+    console.log("I numeri sono uguali.");
+  }
