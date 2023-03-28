71c0cd070dc87e0c1cb590a97824b76db7387577
Federico Airoldi <airoldibusiness@gmail.com>
2023-03-28 11:00:54 +0200

    snack 1 - concluso

--- jsnak1/snack1.js
+++ jsnak1/snack1.js
@@ -7,6 +7,10 @@ let numero1 = prompt("Inserisci il primo numero:");
 // chiedi all'utente di inserire il secondo numero
 let numero2 = prompt("Inserisci il secondo numero:");
 
+// converti i valori inseriti in numeri
+numero1 = parseFloat(numero1);
+numero2 = parseFloat(numero2);
+
 // controlla quale numero è maggiore
 if (numero1 > numero2) {
     console.log("Il numero maggiore è: " + numero1);
